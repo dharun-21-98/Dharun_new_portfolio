9f3cb66d409063d89839f21181a29a02bdac91d4
dharun-21-98 <rdharunanand@gmail.com>
2026-08-14 13:23:18 +0530

    feat: add @vercel/analytics dependency to package.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Syne, Outfit } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const syne = Syne({
   variable: "--font-heading",
